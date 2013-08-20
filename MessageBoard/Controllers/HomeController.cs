5388a3b29939a2db21fb3d4aad4d4b84766cfd4a
Oberdan Bitencourt Ferreira <oberdan.bitencourt@gmail.com>
2013-08-20 00:16:01 -0300

    Fist commit for MessageBoard. Following PluralSight "Building a Site with Bootstrap, AngularJS, ASP.NET, EF and Azure" course.

---
+++ MessageBoard/Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MessageBoard.Models;
+using MessageBoard.Services;
+
+namespace MessageBoard.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IMailService _mailService;
+
+        public HomeController(IMailService mailService)
+        {
+            _mailService = mailService;
+        }
+
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your app description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Contact(ContactModel model)
+        {
+            var message = string.Format("Comment from: {1}{0} Email: {2}{0}WebSite: {3}{0}Comment{4}",
+                                        Environment.NewLine,
+                                        model.Name,
+                                        model.Email,
+                                        model.Website,
+                                        model.Comment);
+            if (_mailService.SendMail("noreply@domain.com", "foo@domain.com", "Website Contact", message))
+            {
+                ViewBag.MailSent = true;
+            }
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult MyMessages()
+        {
+            return View();
+        }
+    }
+}
---
+++ MessageBoard/Services/MailService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Web;
+
+namespace MessageBoard.Services
+{
+    public class MailService: IMailService
+
+{
+    public bool SendMail(string from, string to, string subject, string body)
+    {
+        try
+        {
+            var message = new MailMessage(from, to, subject, body);
+            var client = new SmtpClient();
+            client.Send(message);
+        }
+        catch (Exception)
+        {
+            //log exceptions is a good practice, isn't it?
+            return false;
+        }
+        return true;
+    }
+}
+}
